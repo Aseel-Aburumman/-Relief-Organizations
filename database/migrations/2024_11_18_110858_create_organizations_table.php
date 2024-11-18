cfe08c6c179e94622de06f7d167ab7fcfa20933c
Aseel Aburumman <aseelaburumman49@gmail.com>
2024-11-18 14:59:02 +0300

    database

--- database/migrations/2024_11_18_110858_create_organizations_table.php
+++ database/migrations/2024_11_18_110858_create_organizations_table.php
@@ -11,14 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('organizations', function (Blueprint $table) {
             $table->id();
-            
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('logo')->nullable();
+            $table->text('description')->nullable();
+            $table->text('contact_info')->nullable();
             $table->timestamps();
             $table->softDeletes(); 
         });
@@ -29,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('organizations');
     }
 };
---
+++ database/migrations/2024_11_18_112846_create_needs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('needs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('organization_id')->constrained('organizations')->onDelete('cascade');
+            $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
+            $table->string('item_name');
+            $table->integer('quantity_needed');
+            $table->text('description')->nullable();
+            $table->enum('urgency', ['High Priority', 'Medium Priority']);
+            $table->enum('status', ['Available', 'Partially Fulfilled', 'Fulfilled'])->default('Available');
+            $table->timestamps();
+            $table->softDeletes(); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('needs');
+    }
+};
