a2136da406b1974e011815622976353afff9e604
Aseel Aburumman <aseelaburumman49@gmail.com>
2024-11-20 12:55:52 +0300

    fixes

--- app/Http/Controllers/Orgnization/OrgnizationController.php
+++ app/Http/Controllers/Orgnization/OrgnizationController.php
@@ -4,4 +4,12 @@ namespace App\Http\Controllers\Orgnization;
 
 use App\Http\Controllers\Controller;
 
-class OrgnizationController extends Controller {}
+class OrgnizationController extends Controller
+{
+
+
+    public function dashboard()
+    {
+        return view('orgnization.dashboard');
+    }
+}
