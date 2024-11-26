bee3272660c7db7e02b82fb0b65ff35e7de151ca
talaalsmasi <tala.alsmady96@yahoo.com>
2024-11-26 14:45:28 +0300

    about update

--- resources/views/About.blade.php
+++ resources/views/About.blade.php
@@ -3,78 +3,30 @@
 @section('content')
 <style>
     .help_content{
-        height: 35vh;
+        height: 25vh;
         width: 35vh;
     }
-</style>
-    <!-- bradcam_area_start  -->
-    <div class="bradcam_area breadcam_bg overlay d-flex align-items-center justify-content-center">
-        <div class="container">
-            <div class="row">
-                <div class="col-xl-12">
-                    <div class="bradcam_text text-center">
-                        <h3>About US</h3>
-                    </div>
-                </div>
-            </div>
+</style><br><br><br>
+<div class="row justify-content-center">
+    <div class="col-lg-6">
+        <div class="section_title text-center mb-55">
+            <h3><span>About Website</span></h3>
         </div>
     </div>
+</div>
     <!-- bradcam_area_end  -->
-
-    <!-- reson_area_start  -->
-    <div class="reson_area section_padding">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-lg-6">
-                    <div class="section_title text-center mb-55">
-                        <h3><span>About Website</span></h3>
-                    </div>
-                </div>
-            </div>
-            <div class="row justify-content-center">
-                <div class="col-lg-4 col-md-6">
-                    <div class="single_reson">
-                        <div class="thum">
-                            <div class="thum_1">
-                                <img src="img/help/1.png" alt="">
-                            </div>
-                        </div>
-                        <div class="help_content">
-                            <p>The website showcases charitable organizations with full information about each one.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6">
-                    <div class="single_reson">
-                        <div class="thum">
-                            <div class="thum_1">
-                                <img src="img/help/2.png" alt="">
-                            </div>
-                        </div>
-                        <div class="help_content"><br>
-                            <p>Users can easily view all the listed needs on the platform.</p><br>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6">
-                    <div class="single_reson">
-                        <div class="thum">
-                            <div class="thum_1">
-                                <img src="img/help/3.png" alt="">
-                            </div>
-                        </div>
-                        <div class="help_content">
-                            <p>Users can follow posts documenting the delivery of the listed needs.</p>
+    <div class="slider_area">
+        <div class="single_slider d-flex align-items-center slider_bg_1 overlay2" style="  background-image: url(../img/banner/banner2.jpg);">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-9">
+                        <div class="slider_text">
+                            <h3 style="font-size: 40px">The website highlights charitable organizations, providing full information about each one, while allowing users to easily explore all listed needs and follow posts documenting their delivery.
+                            </h3>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    <!-- reson_area_end  -->
-    <br><br><br><br>
-
-
-
-
+    </div> <br><br>
 @endsection
